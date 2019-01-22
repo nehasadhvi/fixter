aa0fa60ca4ae6a6064f636d402d40f74bf8f2c7d
Neha Sadhvi <ns.sadhvi@yahoo.com>
2019-01-22 14:56:36 -0800

    Initial Commit

---
+++ Pods/Target Support Files/Pods-fixterUITests/Pods-fixterUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_fixterUITests : NSObject
+@end
+@implementation PodsDummy_Pods_fixterUITests
+@end
---
+++ fixter/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  fixter
+//
+//  Created by Neha Sadhvi on 22/01/19.
+//  Copyright © 2019 Neha Sadhvi. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
