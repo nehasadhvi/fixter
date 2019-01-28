87395f99cf99bd9c1eeb3a01a4c0dd8e9f914818
Neha Sadhvi <ns.sadhvi@yahoo.com>
2019-01-28 00:15:13 -0800

    Added Collection View

---
+++ fixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  fixter
+//
+//  Created by Neha Sadhvi on 27/01/19.
+//  Copyright © 2019 Neha Sadhvi. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
