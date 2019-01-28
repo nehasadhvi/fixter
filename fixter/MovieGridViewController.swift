3380a641bc58a954db8fbd2542529f848821baf7
Neha Sadhvi <ns.sadhvi@yahoo.com>
2019-01-27 23:21:30 -0800

    Added movie detail view controller

---
+++ fixter/MovieGridViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieGridViewController.swift
+//  fixter
+//
+//  Created by Neha Sadhvi on 27/01/19.
+//  Copyright © 2019 Neha Sadhvi. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
